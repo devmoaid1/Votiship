d633c4c9bcf9ea9d93c47929037b2bd16e660252
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-09-27 03:03:30 +0800

    added model

---
+++ lib/data/models/president.dart
@@ -0,0 +1,36 @@
+class president {
+  String description;
+  String avatar;
+  String name;
+  String location;
+  int votes;
+  String id;
+
+  president(
+      {this.description,
+      this.avatar,
+      this.name,
+      this.location,
+      this.votes,
+      this.id});
+
+  president.fromJson(Map<String, dynamic> json) {
+    description = json['description'];
+    avatar = json['avatar'];
+    name = json['name'];
+    location = json['location'];
+    votes = json['votes'];
+    id = json['id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['description'] = this.description;
+    data['avatar'] = this.avatar;
+    data['name'] = this.name;
+    data['location'] = this.location;
+    data['votes'] = this.votes;
+    data['id'] = this.id;
+    return data;
+  }
+}
